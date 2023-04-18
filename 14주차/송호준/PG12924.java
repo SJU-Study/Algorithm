b99f9e45a7e38e3f9307069464bf11bbca06a301
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-04-18 09:37:23 +0900

    Create PG12924.java

---
+++ 14주차/송호준/PG12924.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 1; i <= n / 2; i++){
+            int sum = 0;
+            int num = i;
+            while(sum <= n){
+                sum += num++;
+                if(sum == n){ 
+                    answer++;
+                }
+            }
+        }
+        return answer + 1;
+    }
+}
