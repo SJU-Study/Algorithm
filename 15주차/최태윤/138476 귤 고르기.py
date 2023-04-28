1665455f7ba9db6a8d3d4546b21652333e8e6c97
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-04-28 15:52:47 +0900

    Create 138476 귤 고르기.py

---
+++ 15주차/최태윤/138476 귤 고르기.py
@@ -0,0 +1,33 @@
+import sys
+from collections import defaultdict
+
+
+def solution(k, tangerine):
+    # 1 2 2 3 3 4 5 5 
+    # step 1: len(tangerine) - k 만큼 제외한 귤의 종류가 최소가 되도록
+    count_dict = defaultdict(int)
+    
+    for el in tangerine:
+        count_dict[el] += 1
+    
+    # step 2: 큰 거부터 검사
+    v = list(count_dict.values())
+    v.sort(reverse=True)
+    start = 0
+    end = 0
+
+    _sum = v[start]
+    count = sys.maxsize
+    
+    while start <= end and end < len(v):
+        if _sum >= k:
+            # step 3: 큰 것부터 검사했기에 처음 조건에 맞는 케이스가 가장 종류가 적은 것
+            count = min(count, end - start + 1)
+            return count
+            
+        if end + 1 < len(v):
+            end += 1
+            _sum += v[end]
+            continue
+            
+        break
