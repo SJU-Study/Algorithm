b19c6dde1da7faeaf40ddb0d234af9366272332e
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-04 20:41:54 +0900

    feat: 131127 - 할인 행사

---
+++ 16주차/131127.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        int len = number.length;
+
+        // 시작일부터 10일동안 할인하는 제품들 수 확인
+        for (int i = 0; i < discount.length - len; i++) {
+            Map<String, Integer> discountProduct = new HashMap<>();
+            for (int j = 0; j < 10; j++) {
+                if (i + j == discount.length) break;
+                String todayFood = discount[i + j];
+                discountProduct.put(todayFood, discountProduct.getOrDefault(todayFood, 0) + 1);
+            }
+            if (canRegist(want, number, discountProduct)) answer++;
+        }
+        return answer;
+    }
+
+    private static boolean canRegist(String[] want, int[] number, Map<String, Integer> discountProduct) {
+        for (int i = 0; i < want.length; i++) {
+            String product = want[i];
+            if (!discountProduct.containsKey(product) || discountProduct.get(product) < number[i]) return false;
+        }
+        return true;
+    }
+}
