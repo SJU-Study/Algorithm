53de10189d3e1c41beacb4e96efa736613eccae3
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-16 02:25:12 +0900

    Create 68645 삼각 달팽이.py

---
+++ 18주차/최태윤/68645 삼각 달팽이.py
@@ -0,0 +1,39 @@
+from itertools import chain
+def solution(n):
+    answer = [[0] * i for i in range(n + 1)]
+    max_num = (n * (n + 1)) // 2
+    
+
+    currentNum = 1
+    startidx = 0
+    startline = 1
+    endline = n - 1
+    size = n
+    
+    while True:
+        if currentNum == max_num + 1:
+            break
+            
+        for i in range(startline, endline + 2):
+            answer[i][startidx] = currentNum
+            currentNum += 1
+            if currentNum == max_num + 1:
+                break
+        
+        for i in range(startidx + 1, startidx + size):
+            answer[endline+1][i] = currentNum
+            currentNum += 1
+            if currentNum == max_num + 1:
+                break
+        
+        for i in range(endline, startline, -1):
+            answer[i][len(answer[i]) - 1 - answer[i][::-1].index(0)] = currentNum
+            currentNum += 1
+            if currentNum == max_num + 1:
+                break
+        startidx += 1
+        startline += 2
+        endline -= 1
+        size -= 3
+    
+    return list(chain(*answer))
