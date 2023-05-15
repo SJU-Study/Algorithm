cdbd1f7fabb530cd64117dcbcef4f1a7ef1fb848
Taeyun Choi <109710879+cxxxtxxyxx@users.noreply.github.com>
2023-05-15 15:50:14 +0900

    Create 77885 2개 이하로 다른 비트.py

---
+++ 17주차/77885 2개 이하로 다른 비트.py
@@ -0,0 +1,14 @@
+def solution(numbers):
+    answer = []
+    
+    for number in numbers:
+        binary_str = "0b" + "0" * 10 + bin(number)[2:]
+        if binary_str[-1] == '0':
+            answer.append(int(binary_str[:-1] + '1', 2))
+            continue
+        idx = binary_str.rfind("01")
+        answer.append(int(binary_str[:idx] + "10" + binary_str[idx + 2:], 2))
+            
+            
+    return answer
+            
