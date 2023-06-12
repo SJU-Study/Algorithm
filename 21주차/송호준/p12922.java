e6c47a472ee88c6d79406cd1fdf5f4cecad85e6e
Hojun Song <99067128+hojunking96@users.noreply.github.com>
2023-06-12 22:33:59 +0900

    Update p12922.java

--- 21주차/송호준/p12922.java
+++ 21주차/송호준/p12922.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(int n) {
         String answer = "";
         for(int i = 1; i <= n; i++){
-            if(i % 2 == 0){
+            if(i % 2 == 0){  
                 answer += "박";
             }
             else{
